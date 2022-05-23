01054f74614103d54664a9d0a6774a07400d82bf
BrezeanuGabriela <100274462+BrezeanuGabriela@users.noreply.github.com>
2022-05-23 11:32:58 +0300

    Update IComanda.cs

--- Comanda/Comanda/IComanda.cs
+++ Comanda/Comanda/IComanda.cs
@@ -1,17 +1,14 @@
-﻿using System;
+using System;
 
 namespace Restaurant
 {
     public abstract class IComanda
     {
-        protected int _idClient;
-        protected int _idComanda;
-
-        protected static int _nrComenzi = 0;
-
         public abstract double Facturare();
 
         public abstract String AfiseazaComanda();
+
+        public abstract String TiparesteBon();
     }
 }
 
